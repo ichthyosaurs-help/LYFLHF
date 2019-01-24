41acf35b6a54e47e18b40c6479bf09b34f9a045a
Lyf99 <45376521+Lyf99@users.noreply.github.com>
2019-01-24 08:54:01 +0800

    第一次提交

---
+++ app/src/main/java/com/bw/movie/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bw.movie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //十分好看身份卡萨积分兑换
+    }
+}
