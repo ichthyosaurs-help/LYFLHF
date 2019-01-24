a7eb0618be8e9822e3be6ed90e997387ea495720
Lyf99 <45376521+Lyf99@users.noreply.github.com>
2019-01-24 16:25:10 +0800

    补充

--- app/src/main/java/com/bw/movie/utils/SharedUtil.java
+++ app/src/main/java/com/bw/movie/utils/SharedUtil.java
@@ -9,17 +9,44 @@ import android.content.SharedPreferences;
  * <p>创建时间：2019/1/2 000220:47<p>
  */
 public class SharedUtil {
-    private static final String NAME = "user";
+    private static final String NAME = "MOVIE";
+    private static SharedPreferences.Editor editor;
+    private static SharedPreferences sharedPreferences;
+    private static Context mcontext;
+    private static final int TYPE_ONE = 1;
+    private static final int TYPE_TWO = 2;
+    private static final int TYPE_THREE = 3;
 
-    public static void setSave(Context context, String key, String value) {
-        SharedPreferences sharedPreferences = context.getSharedPreferences(NAME, Context.MODE_PRIVATE);
-        SharedPreferences.Editor edit = sharedPreferences.edit();
-        edit.putString(key, value);
-        edit.commit();
+    static class Shared {
+        private static final SharedUtil SHARED_UTIL = new SharedUtil();
     }
 
-    public static String getData(Context context, String key) {
-        SharedPreferences sharedPreferences = context.getSharedPreferences(NAME, Context.MODE_PRIVATE);
-        return sharedPreferences.getString(key, "");
+    //初始化
+    public static SharedUtil SharedPrefrenceHelper(Context context) {
+        mcontext = context;
+        sharedPreferences = context.getSharedPreferences(NAME, Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        return Shared.SHARED_UTIL;
+    }
+
+    public void setSave(String key, Object object) {
+        if (object instanceof String) {
+            editor.putString(key, (String) object);
+        } else if (object instanceof Integer) {
+            editor.putInt(key, (int) object);
+        } else if (object instanceof Boolean) {
+            editor.putBoolean(key, (boolean) object);
+        }
+        editor.commit();
+    }
+
+    public Object getData(String key, int type) {
+        if (type == TYPE_ONE) {
+            return sharedPreferences.getString(key, "");
+        } else if (type == TYPE_TWO) {
+            return sharedPreferences.getInt(key, 0);
+        } else {
+            return sharedPreferences.getBoolean(key, false);
+        }
     }
 }
